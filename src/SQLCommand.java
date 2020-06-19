c929eb1a0ccdaa5b83469c2fd579c93d161ec228
seongwonJeong <rozencrants@naver.com>
2020-06-19 20:01:27 +0900

    버그 수정

--- src/SQLCommand.java
+++ src/SQLCommand.java
@@ -20,7 +20,7 @@ public class SQLCommand {
 		try {
 			ArrayList<Hospital> result = new ArrayList<Hospital>();
 			
-			ResultSet rs = state.executeQuery("select H.id, H.name, H.address, H.phone from Hospital H where H.name LIKE " + hospitalName);
+			ResultSet rs = state.executeQuery("select H.id, H.name, H.address, H.phone from Hospital H where H.name LIKE '%" + hospitalName+ "%'");
 			while(rs.next()) {
 				Hospital hospital = new Hospital(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
 				result.add(hospital);
@@ -29,15 +29,15 @@ public class SQLCommand {
 			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
-		return null;
 	}
 	
 	public ArrayList<Ingredient> searchIngredient(String IngredientName){
 		try {
 			ArrayList<Ingredient> result = new ArrayList<Ingredient>();
 			
-			ResultSet rs = state.executeQuery("select I.id, I.name, I.eat, I.explain from Ingredient I where I.name LIKE " + IngredientName);
+			ResultSet rs = state.executeQuery("select I.id, I.name, I.eat, I.explain from Ingredient I where I.name LIKE '%" + IngredientName + "%'");
 			while(rs.next()) {
 				Ingredient ingredient = new Ingredient(rs.getInt(1),rs.getString(2), rs.getInt(3), rs.getString(4));
 				result.add(ingredient);
@@ -54,7 +54,7 @@ public class SQLCommand {
 		try {
 			ArrayList<Symptom> result = new ArrayList<Symptom>();
 			
-			ResultSet rs = state.executeQuery("select S.id, S.name from Symptom S where S.name LIKE " + SymptomName);
+			ResultSet rs = state.executeQuery("select S.id, S.name from Symptom S where S.name LIKE '%" + SymptomName +"%'");
 			while(rs.next()) {
 				Symptom symptom = new Symptom(rs.getInt(1), rs.getString(2));
 				result.add(symptom);
